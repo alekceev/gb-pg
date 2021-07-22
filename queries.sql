57775ae5119f6983439bfd30e5ebcd2daa405411
Denis Alexeev <alekceeb@gmail.com>
2021-07-22 07:19:53 +0300

    #3 обновил схему и запросы после ревью

--- queries.sql
+++ queries.sql
@@ -2,7 +2,7 @@
 select uuid, name from users where email = 'user1@mail.ru';
 
 -- get user lists
-select * from users_lists u join lists l on u.list_id = l.id where u.user_uuid = (select uuid from users where email = 'user1@mail.ru' limit 1);
+select * from lists l join users u on u.uuid = l.user_uuid where u.uuid = '2d9e428c-ca39-4b55-a2cf-98e068aebbb9';
 
 -- get user lists and items
 with list as (
@@ -10,13 +10,4 @@ with list as (
     where user_uuid in (select uuid from users where email = 'user1@mail.ru')
 )
 select i.*, l.title from lists l join items i on l.id = i.list_id
-where l.id in (select list_id from list);
-
-
--- get user lists and items V2
-with l as (
-    select * from lists l
-    join users_lists u on u.list_id = l.id 
-    where u.user_uuid = (select uuid from users where email = 'user1@mail.ru' limit 1)
-)
-select i.*, l.title from l join items i on l.id = i.list_id;
+where l.user_uuid = '2d9e428c-ca39-4b55-a2cf-98e068aebbb9';
